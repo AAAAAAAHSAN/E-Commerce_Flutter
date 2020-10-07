98291f731da3d01f7525e492ce111288832daaa4
AAAAAAAHSAN <bangladeshahsan@gmail.com>
2020-10-07 18:56:19 +0600

    adding buttons in products details page

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -36,8 +36,112 @@ class _ProductDetailsState extends State<ProductDetails> {
                 color: Colors.white,
                 child: Image.asset(widget.product_details_picture),
               ),
+              footer: new Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: new Text(widget.product_details_name,
+                  style: TextStyle(fontWeight: FontWeight.bold,
+                    fontSize: 16
+                  ),),
+
+                  title: new Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: new Text("\$${widget.product_details_old_price}",
+                        style: TextStyle(color: Colors.grey,
+                        decoration: TextDecoration.lineThrough),
+                        )
+                      ),
+                      Expanded(
+                          child: new Text("\$${widget.product_details_price}",
+                          style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),)
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
             //color: Colors.black54,
+          ),
+
+          // -------------the first button
+          Row(
+            children: <Widget>[
+              // ---------- the size button
+              Expanded(
+                child: MaterialButton(onPressed: (){},
+                color: Colors.white,
+                textColor: Colors.grey,
+                  elevation: 0.2,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: new Text("Size"),
+                    ),
+                    Expanded(
+                      child: new Icon(Icons.arrow_drop_down),
+                    ),
+                  ],
+                ),
+                ),
+                
+              ),
+              // ---------- the color button
+              Expanded(
+                child: MaterialButton(onPressed: (){},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: new Text("Color"),
+                      ),
+                      Expanded(
+                        child: new Icon(Icons.arrow_drop_down),
+                      ),
+                    ],
+                  ),
+                ),
+
+              ),
+              // ---------- the quantity button
+              Expanded(
+                child: MaterialButton(onPressed: (){},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: new Text("Qnty"),
+                      ),
+                      Expanded(
+                        child: new Icon(Icons.arrow_drop_down),
+                      ),
+                    ],
+                  ),
+                ),
+
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: MaterialButton(
+                  onPressed: (){},
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  elevation: 2.0,
+                  child: new Text("Buy Now"),
+                ),
+              ),
+              new IconButton(icon: Icon(Icons.add_shopping_cart), color: Colors.red,onPressed: (){},
+              ),
+              new IconButton(icon: Icon(Icons.favorite_border), color: Colors.red,onPressed: (){},
+              )
+            ],
           )
         ],
       ),
